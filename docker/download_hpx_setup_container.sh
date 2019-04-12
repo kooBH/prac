b5ca39162b91cf6b87326935ff0f7b1f3d05d649
kooBH <39723411+kooBH@users.noreply.github.com>
2019-04-12 20:27:04 +0900

    Changed Owner

--- docker/download_hpx_setup_container.sh
+++ docker/download_hpx_setup_container.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-FILEID=1aM_vfM13BrkIlIIqc7oHHnb8sN_d3cjG
+FILEID=1Z-FynQ5UvXXX84apLdsL5HROO9tTjO94
 FILENAME=hpx_setup.tar 
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$FILEID -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id="$FILEID -O $FILENAME && rm -rf /tmp/cookies.txt
