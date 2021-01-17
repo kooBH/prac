61a136ec839322b31584b896514d8ae762a0c47d
KooBH <ninie4409@sogang.ac.kr>
2021-01-17 14:38:56 +0900

    update

--- python_split_wav_from_json/process.py
+++ python_split_wav_from_json/process.py
@@ -2,9 +2,11 @@ import os
 import json
 import soundfile as sf
 from tqdm import tqdm
+import numpy as np
 
 input_root='\\\\163.239.192.82\\nas1_data\\DB\\[DB]AV-TR(서울대병원)'
 info = input_root+'/'+'tr_intervals_nocaretaker.json'
+
 
 def split(root,target,info) : 
     data,sr = sf.read(root+'/raw/'+target+'.wav')
@@ -13,26 +15,31 @@ def split(root,target,info) :
         t = i.split('~')
         start = float(t[0])
         stop  = float(t[1])
-        #print(str(start)+' ~ ' + str(stop))
         start_idx = int(start*sr)
         stop_idx = int(stop*sr)
-        #print(str(start_idx)+' ~ ' + str(stop_idx))
-        tmp = data[start_idx:stop_idx][:];
-        sf.write(root+'/'+'nurse'+'/'+target+'_nurse_'+str(cnt)+'.wav',tmp,sr)
+        if cnt == 0 :
+            tmp = data[start_idx:stop_idx][:];
+        else :
+            tmp2= data[start_idx:stop_idx][:];
+            np.concatenate([tmp,tmp2])
         cnt+=1
+    sf.write(root+'/'+'nurse'+'/'+target+'_nurse.wav',tmp,sr)
+        
     cnt = 0  
     for i in info['patient'] :
         t = i.split('~')
         start = float(t[0])
         stop  = float(t[1])
-        #print(str(start)+' ~ ' + str(stop))
         start_idx = int(start*sr)
         stop_idx = int(stop*sr)
-        tmp = data[start_idx:stop_idx][:];
-        #print(str(start_idx)+' ~ ' + str(stop_idx))
-        sf.write(root+'/'+'patient'+'/'+target+'_patient_'+str(cnt)+'.wav',tmp,sr)
+        if cnt == 0 :
+            tmp = data[start_idx:stop_idx][:];
+        else :
+            tmp2= data[start_idx:stop_idx][:];
+            np.concatenate([tmp,tmp2]) 
         cnt+=1
-        
+    sf.write(root+'/'+'patient'+'/'+target+'_patient.wav',tmp,sr)    
+    
 if __name__=='__main__':
     with open(info, "rb") as file_json:
         j = json.load(file_json)
