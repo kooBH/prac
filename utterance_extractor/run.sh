d0d56a7a702940b2879dbc0eba0a94ed64a2708c
kooBH <nine4409@sogang.ac.kr>
2022-04-13 16:54:59 +0900

    20220413 update

--- utterance_extractor/run.sh
+++ utterance_extractor/run.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 #python extractor.py -i <입력폴더> -o <출력폴더>
-python extractor.py -i /home/data/kbh/AIG/help_audio/  -o /home/data/kbh/AIG/help_audio_trim/
+python extractor.py -i /home/data/kbh/AIG/help_audio/  -o /home/data/kbh/AIG/help_audio_trim_pad_3sec/
