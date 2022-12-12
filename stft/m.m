07717a6bfb4e6998284c1b7f224b353a8d673d4a
KooBH <ninie4409@sogang.ac.kr>
2022-12-12 12:13:17 +0900

    update

--- stft/m.m
+++ stft/m.m
@@ -15,4 +15,9 @@ win = win./sqrt(sum(win.^2)/nShift);
 nFrame = fix((length(x)+fftSize)/nShift) - 2;
 
 X = zeros(nFrame, nFreq, 1);
-X = stft(x, fftSize, win, nOverlap, fs).'; % [ frame freq mic ]
+X = stft(x, fftSize, win, nOverlap, fs); % [ frame freq mic ]
+
+L1 = load("librosa_stft_1.mat").data;
+L2 = load("librosa_stft_2.mat").data;
+L3 = load("librosa_stft_3.mat").data;
+L4 = load("librosa_stft_4.mat").data;
