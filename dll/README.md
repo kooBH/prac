720860ba9a269303e53893d7629271e6a59e2957
KooBH <ninie4409@sogang.ac.kr>
2023-02-01 21:13:16 +0900

    DLL

---
+++ dll/README.md
@@ -0,0 +1,2 @@
+https://cmake.org/cmake/help/latest/command/add_library.html#command:add_library  
+
---
+++ dll/export/A.cpp
@@ -0,0 +1,14 @@
+#include "A.h"
+
+
+A::A() {
+  printf("Constructor\n");
+
+}
+A::~A() {
+  printf("Destructor\n");
+}
+
+void A::func() {
+  printf("func\n");
+}
